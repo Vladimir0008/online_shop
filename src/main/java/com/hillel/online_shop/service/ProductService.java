f14210e7ce3f89f2e849f0cff622e9021dadf2c1
YuliiaPtak <yui01122015@gmail.com>
2023-03-28 17:09:46 +0300

    implement ProductService and add modelMapper

--- src/main/java/com/hillel/online_shop/service/ProductService.java
+++ src/main/java/com/hillel/online_shop/service/ProductService.java
@@ -1,14 +1,13 @@
 package com.hillel.online_shop.service;
 
 import com.hillel.online_shop.dto.ProductDTO;
-import com.hillel.online_shop.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    Product getProductById(long id);
-    List<Product> getAllProducts();
-    void createProduct(ProductDTO productDTO);
-    void updateProduct(ProductDTO productDTO);
+    ProductDTO getProductById(long id);
+    List<ProductDTO> getAllProducts();
+    Long createProduct(ProductDTO productDTO);
+    Long updateProduct(ProductDTO productDTO);
     void deleteProduct(long id);
 }
--- src/main/java/com/hillel/online_shop/service/ProductServiceImpl.java
+++ src/main/java/com/hillel/online_shop/service/ProductServiceImpl.java
@@ -3,39 +3,58 @@ package com.hillel.online_shop.service;
 import com.hillel.online_shop.dto.ProductDTO;
 import com.hillel.online_shop.entity.Product;
 import com.hillel.online_shop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
-    @Autowired
-    ProductRepository productRepository;
 
+    private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public Product getProductById(long id) {
-        return null;
+    public ProductDTO getProductById(long id) {
+        return mapProduct(findById(id));
     }
 
     @Override
-    public List<Product> getAllProducts() {
-        return null;
+    public List<ProductDTO> getAllProducts() {
+        return StreamSupport.stream(productRepository.findAll().spliterator(), false)
+                .map(entity -> modelMapper.map(entity, ProductDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void createProduct(ProductDTO productDTO) {
-
-    }
+    public Long createProduct(ProductDTO productDTO) {
+        return productRepository.save(mapProduct(productDTO)).getId();
+    } // TODO: 28.03.23 add checking of product existence
 
     @Override
-    public void updateProduct(ProductDTO productDTO) {
-
-    }
+    public Long updateProduct(ProductDTO productDTO) {
+        return productRepository.save(mapProduct(productDTO)).getId();
+    } // TODO: 28.03.23 add checking of product existence
 
     @Override
     public void deleteProduct(long id) {
+        productRepository.deleteById(id);
+    }
 
+    private ProductDTO mapProduct(Product product) {
+        return modelMapper.map(product, ProductDTO.class);
+    }
+
+    private Product mapProduct(ProductDTO productDTO) {
+        return modelMapper.map(productDTO, Product.class);
+    }
+
+    private Product findById(long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product with id " + id + " not found!"));
     }
 }
