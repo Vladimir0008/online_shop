9c67a3018ab440f2d08f9f6d081178b889cb2cf8
Andrew Antonyuk <antonyuk.andrij@gmail.com>
2023-03-29 15:09:07 +0300

    Add ProductController methods

--- src/main/java/com/hillel/online_shop/controller/ProductController.java
+++ src/main/java/com/hillel/online_shop/controller/ProductController.java
@@ -1,11 +1,11 @@
 package com.hillel.online_shop.controller;
 
 import com.hillel.online_shop.dto.ProductDTO;
-import com.hillel.online_shop.service.ProductService;
 import com.hillel.online_shop.service.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,14 +13,33 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     private final ProductServiceImpl productService;
 
-    @PostMapping("/product")
-    public Long save(@RequestBody ProductDTO productDTO) {
+    @PostMapping("/create")
+    public Long create(@RequestBody ProductDTO productDTO) {
 
         return productService.createProduct(productDTO);
     }
 
-    @GetMapping
-    public String ping() {
-        return "Ping";
+    @DeleteMapping(value = "/delete")
+    public void delete(@RequestParam(value = "id") Long id) {
+
+        productService.deleteProduct(id);
+    }
+
+    @GetMapping(value = "/get_product/{id}")
+    public ProductDTO getProduct(@PathVariable Long id) {
+
+        return productService.getProductById(id);
+    }
+
+    @GetMapping(value = "/get_products")
+    public List<ProductDTO> getProducts() {
+
+        return productService.getAllProducts();
+    }
+
+    @PostMapping("/update")
+    public Long update(@RequestBody ProductDTO productDTO) {
+
+        return productService.updateProduct(productDTO);
     }
 }
